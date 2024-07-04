9f9080c699aacb18ecaa4021152374b68d8f3acc
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-07-04 18:09:33 +0100

    Issues fixed

--- cmd/auchan/main.go
+++ cmd/auchan/main.go
@@ -80,7 +80,6 @@ func main() {
 
 	col := colly.NewCollector()
 	col.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 	// col.SetRequestTimeout(120 * time.Second)
 
 	// col.SetProxyFunc(func(_ *http.Request) (*url.URL, error) {
